4012772d6abf62dd5595798052281b5f8be0e437
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2020-03-27 05:21:34 +0530

    Start frontend

---
+++ client/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      "Asfas"
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
